2c0a0fa7c506360127b6debb0222d7a3d0dfe5b1
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-06-29 19:38:43 -0300

    bring entities from first repo

---
+++ src/Domain/Entities/Sale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Sale
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime SaleDate { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public Client CLient { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public ICollection<SaleLine> SaleLine { get; set; }
+    }
+}
