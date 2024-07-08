fb6a0c4589c2038993f5a58ba8bc2edb6ebeef5a
ValenCueto <valentinocueto2005@gmail.com>
2024-07-08 18:19:41 -0300

    ICartService completed

---
+++ src/Application/Interfaces/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ICartService
+    {
+        Task AddProductToCart(int clientId, int productId);
+        Task<IEnumerable<ProductDto>> GetCartProducts(int clientId);
+
+    }
+}
